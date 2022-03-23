4a1696d0d809cf4caff4875736fadb7a772e25b5
DiegoAsmat12 <diegoas1205@gmail.com>
2022-03-23 15:45:08 -0500

    Ejercicio Pokemon API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [listaPokemon,setListaPokemon] = useState([]);
+  const [called, setCalled] = useState(false);
+  useEffect(() =>{
+    if(called){
+      fetch("https://pokeapi.co/api/v2/pokemon?limit=807")
+      .then(respuesta =>{
+        if(respuesta.ok){
+          return respuesta.json();
+        }
+      })
+      .then((data)=>{
+        setListaPokemon([...data.results]);
+      })
+      .catch((err) =>{
+        console.log(err);
+      })
+    }
+    else{
+      setListaPokemon([]);
+    }
+    
+  },[called])
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={(e) => setCalled(!called)}>{!called? "Fetch Pokemon": "Clear Pokemon"}</button>
+      <ul>
+        {
+          listaPokemon.map((pokemon,indice) => {
+            return(
+              <li key={indice}>{pokemon.name}</li>
+            );
+          })
+        }
+      </ul>
+      
     </div>
   );
 }
